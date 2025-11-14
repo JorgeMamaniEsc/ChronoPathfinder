07c7f8b2b22d5690fe2f5ac5b0a1910896ebfbcf
ALUMNO-D104 <ALUMNO-D104@UCSP.AULAB>
2025-11-14 07:34:46 -0500

    dddd

---
+++ include/Juego.h
@@ -0,0 +1,153 @@
+#ifndef JUEGO_H
+#define JUEGO_H
+#include <iostream>
+
+class Juego
+{
+    public:
+        Juego();
+        virtual ~Juego();
+
+    protected:
+
+    private:
+};
+const int MAX = 100;
+
+class NodoBase {
+protected:
+    int id;
+    bool paradoja;
+public:
+    NodoBase(int id = 0);
+    int getId();
+    bool esParadoja();
+    void setParadoja(bool);
+    virtual void mostrarInfo() = 0;
+    virtual ~NodoBase();
+};
+
+class NodoLista {
+private:
+    int id;
+    NodoLista* sig;
+public:
+    NodoLista(int id);
+    friend class ListaNodos;
+};
+
+
+class ListaNodos {
+private:
+    NodoLista* inicio;
+public:
+    void agregar(int id);
+    void mostrar();
+    bool contiene(int id);
+    bool vacia();
+    void eliminar(int id);
+};
+
+
+class LineaTemporal : public NodoBase {
+private:
+    int estado;
+    int padre;
+    ListaNodos* hijos;
+    bool _esCheckpoint;
+public:
+    LineaTemporal(int id, int estado, int padre);
+    void agregarHijo(int idHijo);
+    bool verificarParadoja();
+    int getEstado();
+    int getPadre();
+    void mostrarInfo();
+    void setCheckpoint(bool);
+    bool esCheckpoint();
+};
+
+
+class GrafoTemporal {
+private:
+    LineaTemporal lineas[MAX];
+    int totalLineas;
+public:
+    void agregarLinea(int estado, int padre);
+    LineaTemporal* buscarLinea(int id);
+    void conectar(int origen, int destino);
+    void dfs(int inicio);
+    void mostrarGrafo();
+    void generarParadoja(int id);
+    void propagarParadoja(int id);
+    int getCheckpointCercano(int id);
+};
+
+
+class NodoHistorial {
+private:
+    int idLinea;
+    NodoHistorial* ant;
+    bool esCheckpoint;
+public:
+    NodoHistorial(int id);
+    friend class PilaHistorial;
+};
+
+
+class PilaHistorial {
+private:
+    NodoHistorial* tope;
+public:
+    void push(int id);
+    void push(int id, bool checkpoint);
+    int pop();
+    int cima();
+    bool vacia();
+    void mostrar();
+    int volverCheckpoint();
+};
+
+class Jugador {
+private:
+    char nombre[20];
+    int puntaje;
+    int tiempo;
+public:
+    void setDatos(const char* n, int p, int t);
+    void mostrar();
+    int getPuntaje();
+    const char* getNombre();
+};
+
+
+class BaseDeDatosPuntaje {
+private:
+    Jugador jugadores[MAX];
+    int total;
+public:
+    void agregar(const Jugador& j);
+    void guardarArchivo();
+    void cargarArchivo();
+    void mostrarRanking();
+    int mejorPuntaje();
+};
+
+
+class JuegoPrincipal {
+private:
+    GrafoTemporal grafo;
+    PilaHistorial pila;
+    BaseDeDatosPuntaje db;
+    Jugador jugador;
+    int lineaActual;
+    bool fin;
+public:
+    void iniciar();
+    void menu();
+    void procesarDecision();
+    void mostrarEstadoActual();
+    bool verificarFin();
+    void guardarPuntaje();
+};
+
+#endif // JUEGO_H
---
+++ src/Juego.cpp
@@ -0,0 +1,170 @@
+#include "Juego.h"
+
+Juego::Juego()
+{
+    //ctor
+}
+
+Juego::~Juego()
+{
+    //dtor
+}
+
+NodoBase::NodoBase(int id) : id(id), paradoja() {}
+
+int NodoBase::getId() {
+}
+
+bool NodoBase::esParadoja() {
+
+}
+
+void NodoBase::setParadoja(bool p) {
+}
+
+NodoBase::~NodoBase() {}
+
+NodoLista::NodoLista(int id) {
+}
+
+void ListaNodos::agregar(int id) {
+}
+
+void ListaNodos::mostrar() {
+}
+
+bool ListaNodos::contiene(int id) {
+
+}
+
+bool ListaNodos::vacia() {
+
+}
+
+void ListaNodos::eliminar(int id) {
+}
+
+
+LineaTemporal::LineaTemporal(int id, int estado, int padre) : NodoBase(id){
+}
+void LineaTemporal::agregarHijo(int idHijo) {
+}
+
+bool LineaTemporal::verificarParadoja() {
+
+}
+
+int LineaTemporal::getEstado() {
+
+}
+
+int LineaTemporal::getPadre() {
+
+}
+
+void LineaTemporal::mostrarInfo() {
+}
+
+void LineaTemporal::setCheckpoint(bool) {
+}
+
+bool LineaTemporal::esCheckpoint() {
+
+}
+
+
+void GrafoTemporal::agregarLinea(int estado, int padre) {
+}
+
+LineaTemporal* GrafoTemporal::buscarLinea(int id) {
+
+}
+
+void GrafoTemporal::conectar(int origen, int destino) {
+}
+
+void GrafoTemporal::dfs(int inicio) {
+}
+
+void GrafoTemporal::mostrarGrafo() {
+}
+
+void GrafoTemporal::generarParadoja(int id) {
+}
+
+void GrafoTemporal::propagarParadoja(int id) {
+}
+
+int GrafoTemporal::getCheckpointCercano(int id) {
+
+}
+
+
+NodoHistorial::NodoHistorial(int id) {
+}
+
+
+void PilaHistorial::push(int id) {
+}
+
+void PilaHistorial::push(int id, bool checkpoint) {
+}
+
+int PilaHistorial::pop() {
+
+}
+
+int PilaHistorial::cima() {
+
+}
+
+bool PilaHistorial::vacia() {
+
+}
+
+void PilaHistorial::mostrar() {
+}
+
+int PilaHistorial::volverCheckpoint() {
+
+}
+
+
+void Jugador::setDatos(const char* n, int p, int t) {
+}
+
+void Jugador::mostrar() {
+}
+
+int Jugador::getPuntaje() {
+}
+
+const char* Jugador::getNombre() {
+}
+
+void BaseDeDatosPuntaje::agregar(const Jugador& j) {
+}
+
+void BaseDeDatosPuntaje::guardarArchivo() {
+}
+
+void BaseDeDatosPuntaje::cargarArchivo() {
+}
+
+void BaseDeDatosPuntaje::mostrarRanking() {
+}
+
+int BaseDeDatosPuntaje::mejorPuntaje() {}
+
+
+void JuegoPrincipal::iniciar(){}
+
+void JuegoPrincipal::menu() {}
+
+void JuegoPrincipal::procesarDecision() {}
+
+void JuegoPrincipal::mostrarEstadoActual() {}
+
+bool JuegoPrincipal::verificarFin() {}
+
+void JuegoPrincipal::guardarPuntaje() {}
