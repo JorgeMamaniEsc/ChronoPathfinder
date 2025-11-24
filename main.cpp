5ffbe87652b27e68dfddf25a7e5459356e9d6195
JorgeMamaniEsc <152026996+JorgeMamaniEsc@users.noreply.github.com>
2025-11-23 23:11:08 -0500

    main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,43 @@
 #include <iostream>
+#include "RBT.h"
+#include "Menu.h"
+#include "DataStructures.h"
+#include "TwoSAT.h"
+#include "Personaje.h"
+#include "Game.h"
+#define f(i,a,b,c) for(int i=a;i<b;i+=c)
+#define rf(i,a,b,c) for(int i=a;i>=b;i-=c)
+#define endl '\n'
+
 using namespace std;
-#include "Juego.h"
+
+void cleanScreen() {
+    for(int i=0;i<50;i++) cout<<'\n';
+    //system("cls");
+}
+
+
 
 int main()
 {
+    MenuPrincipal menu;
+    Juego game(3, 2);
+    while(true)
+    {
+        menu.mostrar();
+        int op = menu.leerOpcion();
 
+        if(op == 1){
+            cout << "\nIniciando juego...\n\n";
+            game.jugarNivel(0);
+        }else if(op == 2){
+            cout << "\nPuntajes aún no implementados.\n";
+        }else if(op == 3){
+            cout << "Saliendo del juego...\n";
+            break;
+        }else{
+            cout << "Opción inválida.\n";
+        }
+    }
     return 0;
 }
